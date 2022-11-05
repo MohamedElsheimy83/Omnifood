3dcba3e22e2c6f59334aa70679de52ccf3bb6122
Mohamed Elsheimy <102381640+MohamedElsheimy83@users.noreply.github.com>
2022-11-05 11:47:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,6 +88,28 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
+// This works basically like a media query for jQuery, where we can take different actions depending on the screen width
+
+$(window).resize(function () {
+  var nav = $(".js--main-nav");
+
+  var icon = $(".js--nav-icon i");
+
+  if ($(window).width() > 767) {
+    nav.css("display", "block");
+
+    icon.addClass("ion-close-round");
+
+    icon.removeClass("ion-navicon-round");
+  } else {
+    nav.css("display", "none");
+
+    icon.addClass("ion-navicon-round");
+
+    icon.removeClass("ion-close-round");
+  }
+});
+
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
